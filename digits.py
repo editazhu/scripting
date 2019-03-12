d1746a9ccc2dfc1871cae24cca9747bcea965181
edita.zhu <edita.zhu@gmail.com>
2019-03-12 17:13:32 -0400

    First commit: a digit calculator

---
+++ digits.py
@@ -0,0 +1,25 @@
+"""
+A simple numbers calculator.
+"""
+
+print('Staring a calculator, enter everything except numbers to quit.')
+sum = 0
+digits = []
+
+def calc():
+	result = 0	
+	for digit in digits:
+		result += digit
+	return result
+
+while True: 
+	user_input = input("Enter a number: ")
+	try:
+		number = int(user_input)
+		digits.append(number)
+		sum = calc()
+		print('Current sum is: ', sum)
+
+	except ValueError:
+		print("Error")
+		break
