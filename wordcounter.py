5a822ec445359dfa288442b7c5eb0dd4257be74e
edita.zhu <edita.zhu@gmail.com>
2019-03-13 10:45:24 -0400

    Reduce special characters

--- wordcounter.py
+++ wordcounter.py
@@ -12,15 +12,14 @@ class WC:
 
 	def counts(self):
 		wc_file = self.open_file(self.filename)
-		
-		words = wc_file.replace(',','').replace('.','').replace('!','').replace('?','').replace(';','').replace('‘','').lower()
-		word_list = words.split()
+		words_only = re.sub('\W+', ' ', wc_file)
+		word_list = words_only.split()
 		words_total = len(word_list)
 		print(words_total, "words in total.")
 		
 		word_freq = list(set(word_list))
-		for word in word_freq:
-			print(word + ": " + str(word_freq.count(word)) + "\n")
+		for word in word_list:
+			print(word + ": " + str(word_list.count(word)) + "\n")
 if __name__ == '__main__':
 	file = WC(sys.argv[1])
 	WC.counts(file)
