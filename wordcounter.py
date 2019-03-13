1f5e2f2356116a3b1b8fe0bad8bc0538c7c7b8f6
edita.zhu <edita.zhu@gmail.com>
2019-03-13 10:30:30 -0400

    words frequency counter

---
+++ wordcounter.py
@@ -0,0 +1,26 @@
+import re
+import sys
+
+class WC:
+	def __init__(self, filename):
+		self.filename = filename
+
+	def open_file(self, filename):
+		with open(filename, 'r') as f:
+			file_contents = f.read()
+			return file_contents
+
+	def counts(self):
+		wc_file = self.open_file(self.filename)
+		
+		words = wc_file.replace(',','').replace('.','').replace('!','').replace('?','').replace(';','').replace('‘','').lower()
+		word_list = words.split()
+		words_total = len(word_list)
+		print(words_total, "words in total.")
+		
+		word_freq = list(set(word_list))
+		for word in word_freq:
+			print(word + ": " + str(word_freq.count(word)) + "\n")
+if __name__ == '__main__':
+	file = WC(sys.argv[1])
+	WC.counts(file)
