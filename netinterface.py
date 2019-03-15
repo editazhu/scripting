36759b641c6f9b489c7e59907a307b2a32020788
edita.zhu <edita.zhu@gmail.com>
2019-03-15 11:57:00 -0400

    Collect interface info save into first column

--- netinterface.py
+++ netinterface.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import sh
-import csv
 import re
 
 class NetInterfaces:
@@ -11,18 +10,14 @@ class NetInterfaces:
 	def __init__(self):
 		pass
 	def read_write():
-		with open("interfaces", 'r') as f_in:
+		with open("interfaces", 'r', newline='') as f_in:
 			net_info = f_in.readlines()
 			with open('inet_status.csv','w') as f_out:
-				pattern = re.compile(r'[a-z0-9]+')
-				inet = set() 
 				for line in net_info:
-					match = re.match(pattern, line)
-					print(match)
-					#return re.match object, how to save result?
-					#writer = csv.writer(f_out, 'wb')
-					#inet.add(writer)
-					#writer.writerow(match[1])
+					match = re.match(r'[a-z0-9]+', line)
+					if match:
+						f_out.write(match.group(0))
+						f_out.write('\n')
 			f_out.close()
 		f_in.close()
 		
